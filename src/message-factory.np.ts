c0f0ffc0eff65593ba2f801a4a2613be213c5588
Kenneth <kenneth.ng.nym@outlook.com>
2024-02-15 19:03:28 +0000

    Remove field numbers from NanoPack schemas

--- src/message-factory.np.ts
+++ src/message-factory.np.ts
@@ -2,18 +2,18 @@
 
 import { NanoBufReader, type NanoPackMessage } from "nanopack"
 
-import { InvokeCallback } from "./native-messages/invoke-callback.np.js"
 import { CreateWindow } from "./windowing/create-window.np.js"
+import { InvokeCallback } from "./native-messages/invoke-callback.np.js"
 
 function makeNanoPackMessage(
 	bytes: Uint8Array,
 ): { bytesRead: number; result: NanoPackMessage } | null {
 	const reader = new NanoBufReader(bytes)
 	switch (reader.readTypeId()) {
-		case 2:
-			return InvokeCallback.fromReader(reader)
 		case 10:
 			return CreateWindow.fromReader(reader)
+		case 2:
+			return InvokeCallback.fromReader(reader)
 		default:
 			return null
 	}
